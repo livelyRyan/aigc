e6911f3abb7b3c97265dcdaf4b28179d8f6c16a0
GitHub Action <action@github.com>
2023-06-29 03:34:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/livelyryan/aigc/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/livelyryan/aigc/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.livelyryan.aigc.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
